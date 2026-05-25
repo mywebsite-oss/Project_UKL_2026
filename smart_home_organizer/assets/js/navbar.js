ed46fa0e9d642ef4dac0f0ba20ef877cbb2bca41
LZA Project <luqmanzahidain@gmail.com>
2026-05-25 20:30:02 +0700

    Delete smart_home_organizer directory

--- smart_home_organizer/assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('.navbar');
-    if (window.scrollY > 20) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
